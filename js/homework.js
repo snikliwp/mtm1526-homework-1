581dd1a0a802651c5c288249d4e02d0d2c950a96
snikliwp <wilk0146@algonquinlive.com>
2012-01-13 13:30:19 -0500

    Fixed up for final submission

--- js/homework.js
+++ js/homework.js
@@ -1,17 +1,25 @@
 // JavaScript Document
-	var name = prompt("Please enter a letter");
-var writeNameMultipleTimes = function() {
+	
+	
+var letter
+while (!letter){
+	letter = prompt("Please enter a letter");
+}
+	
+	
+	
+var writeLetterMultipleTimes = function() {
 	for (var i = 1; i<11; i++) {
 		var c = 0;
 		while (c != i) {
-		document.write(name)
+		document.write(letter)
 		c++;
 		}
 		document.write('<br>');
 	}
 };
 
-writeNameMultipleTimes();
+writeLetterMultipleTimes();
 
 
 
